6190fe67f5c4d93c83255ec27e3080c6934b5eac
Unknown <angus.rigby@gmail.com>
2018-01-07 23:57:14 +0000

    Refactor arg handling to be more robust

--- src/rco-dump.cpp
+++ src/rco-dump.cpp
@@ -12,16 +12,39 @@ int main(int argc, char *argv[])
 	FILE *f;
 	std::string file;
 	std::string outdir = ".";
-	
-	if (argc > 1)
-		file = argv[1];
 
-	if (argc > 2)
-		outdir = argv[2];
+	// Loop over arguments and handle accordingly.
+	// -fINPUT_FILE
+	// -dOUTPUT_DIR
+	for (int i = 0; i < argc; i++)
+	{
+		if (argv[i] != nullptr)
+		{
+			int arglen = strlen(argv[i]);
+			if (arglen > 2)
+			{
+				if (argv[i][0] == '-' && argv[i][1] == 'f')
+				{
+					file = &argv[i][2];
+				}
+				else if (argv[i][0] == '-' && argv[i][2] == 'd')
+				{
+					outdir = &argv[i][2];
+				}
+			}
+		}
+	}
 
-
+	// If no -f or -d, attempt to parse any >1 args as input files, 
+	// allows to keep supporting drag&drop on windows
 	if (file.empty())
-		return 1;
+	{
+		if (argc > 1)
+			file = argv[1];
+		
+		if (file.empty())
+			return 1;	
+	}
 
 	f = fopen(file.c_str(), "rb");
 	if (!f)
