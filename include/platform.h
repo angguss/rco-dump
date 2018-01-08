01baf65ddaa71873e037865891bb50e978663685
angguss <angus.rigby@gmail.com>
2018-01-08 00:03:50 +0000

    Move UFT16->UTF8 conversion to platform header

--- include/platform.h
+++ include/platform.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <codecvt>
+#include <locale>
 
 #ifdef _WIN32
 #include <direct.h>
@@ -23,5 +25,17 @@ inline void writeIndent(FILE *f, int depth)
 	if (!f) return;
 	for (unsigned int i = 0; i < depth; i++)
 		fprintf(f, "\t");
+}
+
+inline std::string utf16_to_utf8(std::u16string str)
+{
+#ifdef _WIN32
+	std::wstring_convert<std::codecvt_utf8_utf16<int16_t>, int16_t> convert;
+	auto p = reinterpret_cast<const int16_t *>(str.data());
+	return convert.to_bytes(p);
+#else
+	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
+	return convert.to_bytes(str.data());
+#endif
 }
 
